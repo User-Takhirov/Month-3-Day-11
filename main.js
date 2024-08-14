c3f884b29f67e951f26ffd22974c98c1ebd84ccc
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-14 18:37:20 +0500

    js day-11

---
+++ main.js
@@ -0,0 +1,118 @@
+// let studentData = [12, 24, ["jack", 24], null, ["sara", 18], "lorem"];
+
+// let data = [];
+
+// const x = (arr) => {
+//   for (let i of arr) {
+//     data.push(i);
+//   }
+// };
+
+// for (let i of studentData) {
+//   if (typeof i == "object" && i?.length) {
+//     x(i);
+//   }
+// }
+// console.log(data);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// yoshlarni bitta arrayga push qilish
+// let arr = [
+//   {
+//     name: "John",
+//     age: 21,
+//   },
+//   {
+//     name: "Alex",
+//     age: 29,
+//   },
+//   {
+//     name: "Cena",
+//     age: 11,
+//   },
+// ];
+// let ages = [];
+
+// for (let i in arr) {
+//   ages.push(arr[i].age);
+// }
+
+// console.log(ages);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// homework
+///1
+// const obj = {
+//   name: "John",
+//   age: 30,
+//   address: {
+//     street: "123 Main St",
+//     city: "New York",
+//     zip: "10001",
+//   },
+//   contact: {
+//     phone: "555-555-5555",
+//     email: "john@example.com",
+//   },
+// };
+// const newArr = [];
+// for (const i in obj) {
+//   if (typeof
+//     obj[i]== 'object' &&
+//     typeof obj[i] === "object" &&
+//     obj[i] !== null
+//   ) {
+//     newArr.push(obj[i]);
+//   }
+// }
+// console.log(newArr);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//2
+// let arr = [
+//   1,
+//   3,
+//   "lorem",
+//   5,
+//   null,
+//   { name: "Alexa", age: 21 },
+//   { name: "Javohir", age: 18 },
+// ];
+// function x(arr) {
+//   let newArr = [];
+//   for (let i of arr) {
+//     if (typeof i === "object" && i !== null) {
+//       newArr.push(i);
+//     }
+//   }
+//   return newArr;
+// }
+// console.log(x(arr));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//3
+// let key = prompt("keyWords kiriting");
+// let arr = [
+//   {
+//     name: "John",
+//     age: 21,
+//     id: 3,
+//   },
+//   {
+//     name: "Alex",
+//     age: 29,
+//     id: 2,
+//   },
+//   {
+//     name: "Cena",
+//     age: 11,
+//     id: 1,
+//   },
+// ];
+// let ages = [];
+// for (let i in arr) {
+//   ages.push(arr[i][key]);
+// }
+
+// console.log(ages);
